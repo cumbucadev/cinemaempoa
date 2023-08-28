e2b17673035cec48e04e3c49ea3a2d5b34c0cbc3
Guilherme Garcia <gui.garcia67@gmail.com>
2023-08-28 08:29:14 -0300

    Fix date format in header and html structure

--- build.py
+++ build.py
@@ -7,34 +7,49 @@ from datetime import datetime
 
 
 def generate_movie_grid(cinema):
-    html = (
-        "<div class='cinema'><h2>"
-        + cinema["cinema"]
-        + " <small><a href="
-        + cinema["url"]
-        + ">ver site</a></small></h2><ul>"
-    )
+    movies_list = ""
     for item in cinema["features"]:
         if "poster" in item and item["poster"] != "":
-            html += '<li style="min-height: 325px;">'
-            html += f"<img src=\"{item['poster']}\" width=225 loading=\"lazy\" alt=\"{item['title']}\">"
+            movies_list += '<li style="min-height: 325px;">'
+            movies_list += f"<img src=\"{item['poster']}\" width=225 loading=\"lazy\" alt=\"{item['title']}\">"
         else:
-            html += "<li>"
-        html += f"""
+            movies_list += "<li>"
+        movies_list += f"""
                 <h3>{item['title']}</h3>
                 <p>{item['general_info']}</p>
                 <p>{item['time']}</p>
         """
         if "direction" in item and item["director"] is not False:
-            html += f"<p>{item['director']}</p>"
+            movies_list += f"<p>{item['director']}</p>"
 
-        html += f"""
+        movies_list += f"""
                 <p>{item['excerpt']}</p>
-                <a href="{item['read_more']}">Read More</a>
+                <a href="{item['read_more']}">Mais informações</a>
             </li>
         """
+    warnings = ""
+    if movies_list == "":
+        warnings = "<strong style='color: red;'>Não há sessões hoje</strong><br/>"
+    if "warning" in cinema and cinema["warning"]:
+        warnings += f"<strong style='color: red;'>{cinema['warning']}</strong>"
+    html = f"""
+        <article>
+            <header>
+                <h2>
+                    {cinema['cinema']}
+                    <small class='cinema-url'>
+                        <a href="{cinema['url']}">visitar</a>
+                    </small>
+                </h2>
+                {warnings}
+            </header>
+            <main>
+                <ul>
+                    {movies_list}
+                </ul>
+            </main>
+        </article>"""
 
-    html += "</ul></div>"
     return html
 
 
@@ -48,13 +63,15 @@ def generate_html_structure(content):
             <title>CINEMA EM POA</title>
             <style>
                 img {{ float: left; max-height: 325px; object-fit: contain; margin-right: 15px; }}
+                ul {{ list-style: none; }}
+                .cinema-url {{ font-weight: normal; font-size: 1rem; }}
             </style>
         </head>
         <body>
             <header>
                 <h1>CINEMA EM POA</h1>
                 <p>Este site mostra os filmes em cartaz em algumas das diversas salas de cinema de Porto Alegre.</p>
-                <p>Mostrando filmes para <strong>{datetime.now().date()}</strong></p>
+                <p>Mostrando filmes para <strong>{datetime.now().strftime("%d/%m/%Y")}</strong></p>
             </header>
             {"".join(content)}
         </body>
@@ -68,8 +85,6 @@ json_data = json.loads(incoming_data)
 
 movie_grids = []
 for cinema in json_data:
-    if len(cinema["features"]) == 0:
-        continue
     movie_grid = generate_movie_grid(cinema)
     movie_grids.append(movie_grid)
 
