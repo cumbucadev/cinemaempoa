3afd65244ba92d97996d0d7e2fc184660d8c26d9
Guilherme Garcia <gui.garcia67@gmail.com>
2023-08-28 22:58:13 -0300

    Cache images locally when generating html

--- build.py
+++ build.py
@@ -1,17 +1,45 @@
 #!/usr/bin/env python
 
+import hashlib
 import json
+import requests
+import math
+import os
 import sys
 
 from datetime import datetime
+from io import BytesIO
+from PIL import Image
 
 
 def generate_movie_grid(cinema):
     movies_list = ""
     for item in cinema["features"]:
         if "poster" in item and item["poster"] != "":
-            movies_list += '<li style="min-height: 325px;">'
-            movies_list += f"<img src=\"{item['poster']}\" width=225 loading=\"lazy\" alt=\"{item['title']}\">"
+            # attempt to download file locally
+            file_extension = item["poster"].split(".")[-1]
+            file_name = (
+                hashlib.md5(item["poster"].encode("utf-8")).hexdigest()
+                + "."
+                + file_extension
+            )
+            if os.path.exists(file_name):
+                with open(file_name, "rb") as f:
+                    img = Image.open(f)
+            else:
+                r = requests.get(item["poster"])
+                with open(file_name, "wb") as f:
+                    file_content = r.content
+                    f.write(file_content)
+                    img = Image.open(BytesIO(file_content))
+
+            width = img.width
+            height = img.height
+
+            imgDisplayWidth = 325
+            minHeight = math.ceil(imgDisplayWidth / width * height)
+            movies_list += f"<li style='min-height: {minHeight}px;'>"
+            movies_list += f"<img src=\"{file_name}\" width={imgDisplayWidth} loading=\"lazy\" alt=\"{item['title']}\">"
         else:
             movies_list += "<li>"
         movies_list += f"""
@@ -63,8 +91,12 @@ def generate_html_structure(content):
             <title>CINEMA EM POA</title>
             <meta name="description" content="Filmes em cartaz nas salas CineBancários, Cinemateca Paulo Amorim, Cinemateca Capitólio e Sala Redenção de Porto Alegre." />
             <style>
-                img {{ float: left; max-height: 325px; object-fit: contain; margin-right: 15px; }}
+                body {{
+                    max-width: 960px;
+                }}
+                img {{ float: left; max-width: 100%; object-fit: contain; margin-right: 15px; }}
                 ul {{ list-style: none; }}
+                li, p {{ font-size: 1.25rem; }}
                 .cinema-url {{ font-weight: normal; font-size: 1rem; }}
             </style>
         </head>
