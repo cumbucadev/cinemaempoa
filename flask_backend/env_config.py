31d08da655a7ec428a588241bf0442d5ccd587fe
che <che@certi.org.br>
2024-04-22 23:43:25 -0300

    feat: implement upload of imagens into imgbb

--- flask_backend/env_config.py
+++ flask_backend/env_config.py
@@ -6,3 +6,4 @@ APP_ENVIRONMENT = config("APP_ENVIRONMENT", default="development")
 ADMIN_PROD_USERNAME = config("ADMIN_PROD_USERNAME", default="cinemaempoa")
 ADMIN_PROD_PWD = config("ADMIN_PROD_PWD", default="secret-pwd")
 UPLOAD_DIR = config("UPLOAD_DIR", None)
+IMGBB_API_KEY = config("IMGBB_API_KEY", default="invalid-key") # api-key from https://api.imgbb.com/
