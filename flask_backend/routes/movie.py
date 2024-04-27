6f80c75552dea7cd2a472493c3433ddb7ab64b3b
Guilherme Garcia <gui.garcia67@gmail.com>
2024-04-27 16:36:53 -0300

    Test usage of flexbox for movie grid

--- flask_backend/routes/movie.py
+++ flask_backend/routes/movie.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, g, jsonify, render_template, request
+import math
 
-from flask_backend.repository.movies import get_all as get_all_movies, get_paginated
-from flask_backend.repository.movies import get_movies_with_similar_titles
-from flask_backend.routes.auth import login_required
+from flask import Blueprint, g, jsonify, render_template, request
 from werkzeug.exceptions import abort
+
+from flask_backend.repository.movies import get_all as get_all_movies
+from flask_backend.repository.movies import (
+    get_movies_with_similar_titles,
+    get_paginated,
+)
+from flask_backend.routes.auth import login_required
 
 bp = Blueprint("movie", __name__)
 
@@ -16,6 +21,7 @@ def index():
         "movie/index.html", movies=movies, show_drafts=user_logged_in
     )
 
+
 @bp.route("/movies/posters")
 def posters():
     page = request.args.get("page", 0)
@@ -24,12 +30,47 @@ def posters():
     except ValueError:
         abort(400)
     user_logged_in = g.user is not None
-    movies = get_paginated( page, 12, user_logged_in)
+    qtt_movies = 12 if page == 0 else 4
+    movies = get_paginated(page, qtt_movies, user_logged_in)
     if len(movies) == 0:
         abort(404)
 
+    # number of columns in the grid
+    num_columns = 3
+    # limits how wide a movie image can be on the listing
+    imgDisplayWidth = 325
+    images = []
+    for movie in movies:
+        for screening in movie.screenings:
+            if screening.image:
+                images.append(
+                    {
+                        "url": screening.image,
+                        "width": imgDisplayWidth,
+                        "height": math.ceil(
+                            imgDisplayWidth
+                            / screening.image_width
+                            * screening.image_height
+                        ),
+                    }
+                )
+    cols_height = [0] * num_columns
+    for i in range(0, len(images), 3):
+        for j in range(num_columns):
+            try:
+                cols_height[j] = cols_height[j] + int(images[i + j]["height"])
+            except IndexError:
+                cols_height[j] = cols_height[j] + 0
+
+    # take the tallest column and add the equivalent margin-bottom for each row
+    num_rows = math.ceil(len(images) / num_columns)
+    max_column_height = max(cols_height) + (num_rows * 15)
     return render_template(
-        "movie/movies.html", movies=movies, show_drafts=user_logged_in, page=page
+        "movie/movies.html",
+        images=images,
+        max_column_height=max_column_height,
+        show_drafts=user_logged_in,
+        page=page,
     )
 
 
