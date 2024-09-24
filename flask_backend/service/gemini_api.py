0c70dde9bb4ec1b2ff23b739442632a726eb8824
Guilherme Garcia <gui.garcia67@gmail.com>
2024-09-24 18:41:14 -0300

    Change gemini model (gemini-pro-vision seems to be discontinued)

--- flask_backend/service/gemini_api.py
+++ flask_backend/service/gemini_api.py
@@ -15,7 +15,8 @@ class Gemini:
         self.headers = {"Content-Type": "application/json"}
         self.api_key = GEMINI_API_KEY
         self.models = {
-            "gemini-pro-vision": "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision"
+            "gemini-pro-vision": "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro-vision",  # this one seems to be discontinued
+            "gemini-1.5-flash-latest": "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest",
         }
 
     def _get_url(self, model, resource):
@@ -39,7 +40,7 @@ class Gemini:
             ]
         }
         r = requests.post(
-            self._get_url("gemini-pro-vision", "generateContent"),
+            self._get_url("gemini-1.5-flash-latest", "generateContent"),
             json=payload,
             headers=self.headers,
         )
