b5e10ed7a43476945593629464b09a777d870fe8
guites <71985299+guites@users.noreply.github.com>
2024-09-24 22:45:37 -0300

    #60 uso de ia para gerar descricao de imagens (#70)
    * wip: integração inicial com API Gemini IA
    
    * Inserts AI generated alt text into screening form
    
    * Save image alt text on screening creation
    
    * Show poster alt text whenever present
    
    * Persist image alt on validation failure
    
    * Implement alt generation for screening update
    
    * Change gemini model (gemini-pro-vision seems to be discontinued)
    
    * Add gemini key env var to example.env
    
    * Adapt alt text generation for screenings with existing images
    
    * Set BR timezone to docker container

--- flask_backend/env_config.py
+++ flask_backend/env_config.py
@@ -8,4 +8,7 @@ APP_ENVIRONMENT = config("APP_ENVIRONMENT", default=EnvironmentEnum.DEVELOPMENT)
 ADMIN_PROD_USERNAME = config("ADMIN_PROD_USERNAME", default="cinemaempoa")
 ADMIN_PROD_PWD = config("ADMIN_PROD_PWD", default="secret-pwd")
 UPLOAD_DIR = config("UPLOAD_DIR", None)
-IMGBB_API_KEY = config("IMGBB_API_KEY", default="invalid-key") # api-key from https://api.imgbb.com/
+IMGBB_API_KEY = config(
+    "IMGBB_API_KEY", default="invalid-key"
+)  # api-key from https://api.imgbb.com/
+GEMINI_API_KEY = config("GEMINI_API_KEY", None)
