a99032478440b2c6a61a1b28040cd3d30f63df0d
Guilherme Garcia <gui.garcia67@gmail.com>
2024-10-18 19:19:00 -0300

    Remove unused blog templates

--- flask_backend/routes/screening.py
+++ flask_backend/routes/screening.py
@@ -449,13 +449,3 @@ def describe_image():
         )
     image_description = candidate["content"]["parts"][0]["text"]
     return jsonify(text=image_description.strip())
-
-
-# @bp.route("/<int:id>/delete", methods=("POST",))
-# @login_required
-# def delete(id):
-#     get_post(id)
-#     db = get_db()
-#     db.execute("DELETE FROM post WHERE id = ?", (id,))
-#     db.commit()
-#     return redirect(url_for("blog.index"))
