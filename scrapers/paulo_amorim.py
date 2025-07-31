8ed83bfc79511a96e2775f73b8fa1848f40f081e
Guilherme <71985299+guites@users.noreply.github.com>
2025-07-31 11:12:15 -0300

    Bugfix/#130 pa scrapper nao encontra horario dos filmes (#134)
    * Strips weird white space literals from movie titles

--- scrapers/paulo_amorim.py
+++ scrapers/paulo_amorim.py
@@ -267,7 +267,7 @@ class CinematecaPauloAmorim:
                     else:
                         parsed_time = dt_time(int(hour_str))
                     for movie in self.movies:
-                        if movie["title"].lower() != feature_time_match[1].lower():
+                        if movie["title"].strip().lower() != feature_time_match[1].strip().lower():
                             continue
                         movie["time"].append(parsed_time)
 
