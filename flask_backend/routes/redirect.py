fa7df566d0d69caae8c7c18a1275d2963bf3f2a4
Guilherme Garcia <gui.garcia67@gmail.com>
2025-09-03 20:25:45 -0300

    Revert "Adiciona rota de redirecionamento para cumbuca.dev"
    This reverts commit c500af6680d40fb4be1904703af0a53d8cb81875.

--- flask_backend/routes/redirect.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, redirect
-
-redirect_bp = Blueprint("redirect", __name__)
-
-@redirect_bp.route('/cumbuca')
-def redirecionar():
-    return redirect("https://cumbuca.dev", code=302)
