0440bbc4c087c49641e56a9d75781f5cffaab3a6
guites <gui.garcia67@gmail.com>
2025-09-08 16:28:55 -0300

    Include slug in movie seeds

--- flask_backend/seeds/movie_seeds.py
+++ flask_backend/seeds/movie_seeds.py
@@ -3,14 +3,14 @@ from flask_backend.models import Movie
 
 def create_movies(db_session):
     movies = [
-        Movie(title="Lobo e Cão"),
-        Movie(title="Terra de Deus"),
-        Movie(title="MARINHEIRO DAS MONTANHAS"),
-        Movie(title="NARDJES A."),
-        Movie(title="ELIS & TOM – SÓ TINHA DE SER COM VOCÊ"),
-        Movie(title="A CASA DOS PRAZERES"),
-        Movie(title="OLDBOY"),
-        Movie(title="NOSSO SONHO"),
+        Movie(title="Lobo e Cão", slug="lobo-e-cao"),
+        Movie(title="Terra de Deus", slug="terra-de-deus"),
+        Movie(title="MARINHEIRO DAS MONTANHAS", slug="marinheiro-das-montanhas"),
+        Movie(title="NARDJES A.", slug="nardjes-a"),
+        Movie(title="ELIS & TOM – SÓ TINHA DE SER COM VOCÊ", slug="elis-tom-so-havia-de-ser-com-voce"),
+        Movie(title="A CASA DOS PRAZERES", slug="a-casa-dos-prazeres"),
+        Movie(title="OLDBOY", slug="oldboy"),
+        Movie(title="NOSSO SONHO", slug="nosso-sonho"),
     ]
     db_session.add_all(movies)
     db_session.commit()
