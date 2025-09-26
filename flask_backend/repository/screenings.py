d0df266456398401391eed75f1460ee52835eadf
Guilherme Garcia <gui.garcia67@gmail.com>
2025-09-26 13:28:19 -0300

    Remove drafted screenings from weekend page

--- flask_backend/repository/screenings.py
+++ flask_backend/repository/screenings.py
@@ -142,9 +142,10 @@ def delete(
 def get_weekend_screening_dates() -> Tuple[List[ScreeningDate], date, date, date]:
     current_date = date.today()
     friday_date, saturday_date, sunday_date = get_weekend_dates(current_date)
-    # TODO: do not return dates from screenings that are drafted
     return (
         db_session.query(ScreeningDate)
+        .join(Screening)
+        .filter(Screening.draft == False)
         .filter(func.date(ScreeningDate.date).between(friday_date, sunday_date))
         .order_by(func.date(ScreeningDate.date))
         .order_by(func.time(ScreeningDate.time))
