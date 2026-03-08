a32f5b5b9c3f050c056b1383d03621a4e67cd5a6
Gpocas <gpocas01@gmail.com>
2026-03-08 12:03:30 -0300

    fix seed screenings

--- flask_backend/seeds/screening_seeds.py
+++ flask_backend/seeds/screening_seeds.py
@@ -1,39 +1,36 @@
+import random
 from datetime import datetime
 
 from faker import Faker
 
 from flask_backend.models import Screening, ScreeningDate
 
+DEFAULT_WIDTH=303
+DEFAULT_HEIGHT=455
+
 
 def create_screenings(db_session):
     faker = Faker()
-
-    screenings = [
-        Screening(
-            movie_id=1,
-            cinema_id=1,
+    screenings = []
+    for index in range(1, 8):
+        
+        obj = Screening(
+            movie_id=index,
+            cinema_id=random.randint(1, 4),
             url=faker.uri(),
-            # image=...,
+            image=faker.image_url(DEFAULT_WIDTH, DEFAULT_HEIGHT),
+            image_width=DEFAULT_WIDTH,
+            image_height=DEFAULT_HEIGHT,
             description=faker.text(),
             dates=[
                 ScreeningDate(date=datetime.now().date(), time="11:00"),
                 ScreeningDate(date=datetime.now().date(), time="13:00"),
                 ScreeningDate(date=datetime.now().date(), time="17:00"),
             ],
-        ),
-        Screening(
-            movie_id=2,
-            cinema_id=1,
-            url=faker.uri(),
-            # image=..,
-            description=faker.text(),
-            dates=[
-                ScreeningDate(date=datetime.now().date(), time="11:00"),
-                ScreeningDate(date=datetime.now().date(), time="13:00"),
-                ScreeningDate(date=datetime.now().date(), time="17:00"),
-            ],
-        ),
-    ]
+        )
+        
+        screenings.append(obj)
+       
 
     db_session.add_all(screenings)
     db_session.commit()
