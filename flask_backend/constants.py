ab1e7e3558e6362c5dbf06f496dffeb8c11ee51b
Guilherme <71985299+guites@users.noreply.github.com>
2026-07-25 14:14:49 -0300

    refactor: centralize cinema slugs and colors as model properties (#269)

---
+++ flask_backend/constants.py
@@ -0,0 +1,17 @@
+CINEMA_SHORT_NAMES = {
+    "capitolio": "Capitólio",
+    "sala-redencao": "Sala Redenção",
+    "cinebancarios": "CineBancários",
+    "paulo-amorim": "Paulo Amorim",
+    "cine-cinco": "Cine Cinco",
+}
+
+CINEMA_COLORS = {
+    "capitolio": "#911eb4",
+    "sala-redencao": "#000075",
+    "cinebancarios": "#9A6324",
+    "paulo-amorim": "#469990",
+    "cine-cinco": "#e6194b",
+}
+
+DEFAULT_CINEMA_COLOR = "#6c757d"
