852ebe0f3007a0f48d2d22bea5d52b4fc3d5fa9b
guites <gui.garcia67@gmail.com>
2026-07-26 17:49:28 -0300

    feat: uses persistent cookie for admin login (defaults to 1 year)

--- flask_backend/env_config.py
+++ flask_backend/env_config.py
@@ -4,6 +4,7 @@ from flask_backend.utils.enums.environment import EnvironmentEnum
 
 DATABASE_URL = config("DATABASE_URL", default="sqlite:///./development.sqlite")
 SESSION_KEY = config("SESSION_KEY", default="dev")
+SESSION_LIFETIME_DAYS = config("SESSION_LIFETIME_DAYS", default=365, cast=int)
 APP_ENVIRONMENT = EnvironmentEnum(
     config("APP_ENVIRONMENT", default=EnvironmentEnum.DEVELOPMENT)
 )
