bee0e28825460c6a8b3bdaf2080671c6615245c3
guites <gui.garcia67@gmail.com>
2026-07-27 12:09:44 -0300

    feat: track want-to-watch adds via goatcounter event

--- flask_backend/static/reels-want-to-watch.js
+++ flask_backend/static/reels-want-to-watch.js
@@ -32,9 +32,18 @@ document.addEventListener("click", (event) => {
         })
         .then((data) => {
             setWantToWatchState(button, data.wanted);
-            if (data.wanted && !shownAddedHint) {
-                shownAddedHint = true;
-                showAddedToast();
+            if (data.wanted) {
+                if (window.goatcounter) {
+                    window.goatcounter.count({
+                        path: window.location.pathname,
+                        title: "Marked movie as want-to-watch",
+                        event: true,
+                    });
+                }
+                if (!shownAddedHint) {
+                    shownAddedHint = true;
+                    showAddedToast();
+                }
             }
         })
         .catch((error) => {
